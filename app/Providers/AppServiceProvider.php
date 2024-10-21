6f1e499068e83bcb83c6ce767fc75f664e0ae0f2
enginedilema <omiro@xtec.cat>
2024-10-21 22:55:55 +0200

    SQLite optimizations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (config('database.default') === 'sqlite') {
+            DB::statement('PRAGMA journal_mode=WAL;'); // Habilitar WAL para concurrencia
+            DB::statement('PRAGMA synchronous=NORMAL;'); // Sincronización para mejor rendimiento
+            DB::statement('PRAGMA foreign_keys=ON;'); // Habilitar claves foráneas
+            DB::statement('PRAGMA cache_size=10000;'); // Aumentar tamaño de la caché
+        }
     }
 }
--- config/database.php
+++ config/database.php
@@ -37,6 +37,9 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'options'   => [
+                PDO::ATTR_PERSISTENT => true, // Conexiones persistentes activadas
+            ],
         ],
 
         'mysql' => [
